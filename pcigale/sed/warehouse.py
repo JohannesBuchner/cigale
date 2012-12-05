26ae8f5b950456c60dd627c1933ed47a371115bf
Yannick Roehlly <yannick.roehlly@oamp.fr>
2012-12-05 13:15:38 +0100

    Use two list instead of a list of tuples

--- pcigale/sed/warehouse.py
+++ pcigale/sed/warehouse.py
@@ -10,13 +10,17 @@ from . import SED
 from .modules import common as sed_modules
 
 
-def create_sed(module_list):
-    """Create a new SED using the given modules
+def create_sed(module_list, parametre_list):
+    """Create a new SED using the given modules and parametres
 
     Parametres
     ----------
-    module_list :array of tuples
-        Array of tuples (module name, parametre dictionnary).
+    module_list : list
+        List of module names in the order they have to be used to create
+        the SED.
+    parametre_list : list of dictionaries
+        List of the parametre dictionaries corresponding to each module of
+        the module_list list.
 
     Returns
     -------
@@ -28,7 +32,7 @@ def create_sed(module_list):
     # We start from an empty SED.
     sed = SED()
 
-    for (module, parametres) in module_list:
+    for (module, parametres) in zip(module_list, parametre_list):
         mod = sed_modules.get_module(module)
         mod.parametres = parametres
         mod.process(sed)
