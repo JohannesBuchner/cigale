bf48b5dad76b1a3457edd2fcbfb22ae132004a80
Yannick Roehlly <yannick@iaora.eu>
2014-02-04 12:32:18 +0100

    Add some functions to analysis_modules
    Put some function that may be shared between several analysis modules at
    the analysis_modules level.

--- pcigale/analysis_modules/__init__.py
+++ pcigale/analysis_modules/__init__.py
@@ -5,6 +5,9 @@
 
 from importlib import import_module
 from collections import OrderedDict
+import numpy as np
+from scipy import stats
+from astropy.table import Column
 
 
 class AnalysisModule(object):
@@ -154,3 +157,153 @@ def get_module(module_name):
     except ImportError:
         print('Module ' + module_name + ' does not exists!')
         raise
+
+
+def adjust_errors(flux, error, tolerance, default_error=0.1,
+                  systematic_deviation=0.1):
+    """Adjust the errors replacing the 0 values by the default error and
+    adding the systematic deviation.
+
+    The systematic deviation change the error to:
+    sqrt( error² + (flux * deviation)² )
+
+    Parameters
+    ----------
+    flux : array of floats
+        Fluxes.
+    error : array of floats
+        Observational error in the same unit as the fluxes.
+    tolerance : float
+        Tolerance threshold under flux error is considered as 0.
+    default_error : float
+        Default error factor used when the provided error in under the
+        tolerance threshold.
+    systematic_deviation : float
+        Systematic deviation added to the error.
+
+    Returns
+    -------
+    error : array of floats
+        The corrected errors.
+
+    """
+
+    # The arrays must have the same lengths.
+    if len(flux) != len(error):
+        raise ValueError("The flux and error arrays must have the same "
+                         "length.")
+
+    # We copy the error array not to modify the original one.
+    error = np.copy(error)
+
+    # Replace errors below tolerance by the default one.
+    error[error < tolerance] = (default_error * flux[error < tolerance])
+
+    # Add the systematic error.
+    error = np.sqrt(np.square(error) + np.square(flux * systematic_deviation))
+
+    return error
+
+
+def complete_obs_table(obs_table, used_columns, filter_list, tolerance,
+                       default_error=0.1, systematic_deviation=0.1):
+    """Complete the observation table
+
+    For each filter:
+    * If the corresponding error is not present in the used column list or in
+      the table columns, add (or replace) an error column with the default
+      error.
+    * Adjust the error value.
+
+    Parameters
+    ----------
+    obs_table : astropy.table.Table
+        The observation table.
+    used_columns : list of strings
+        The list of columns to use in the observation table.
+    filter_list : list of strings
+        The list of filters used in the analysis.
+    tolerance : float
+        Tolerance threshold under flux error is considered as 0.
+    default_error : float
+        Default error factor used when the provided error in under the
+        tolerance threshold.
+    systematic_deviation : float
+        Systematic deviation added to the error.
+
+    Returns
+    -------
+    obs_table = astropy.table.Table
+        The completed observation table
+
+    Raises
+    ------
+    StandardError : When a filter is not present in the observation table.
+
+    """
+    # TODO Print or log a warning when an error column is in the used column
+    # list but is not present in the observation table.
+    for name in filter_list:
+        if name not in obs_table.columns:
+            raise StandardError("The filter <{}> (at least) is not present in "
+                                "the observation table.".format(name))
+
+        name_err = name + "_err"
+        if name_err not in used_columns:
+            if name_err not in obs_table.columns:
+                obs_table.add_column(Column(
+                    name=name_err,
+                    data=np.zeros(len(obs_table), dtype=float),
+                    index=obs_table.colnames.index(name)+1
+                ))
+            else:
+                obs_table[name_err] = np.zeros(len(obs_table))
+
+        obs_table[name_err] = adjust_errors(obs_table[name],
+                                            obs_table[name_err],
+                                            tolerance,
+                                            default_error,
+                                            systematic_deviation)
+    return obs_table
+
+
+def bin_evenly(values, max_bins):
+    """Divide some values in evenly populated bins
+
+    Given a list of values and a desired number of bins, this method computes
+    the bins boundaries to have bins with the same number of elements in each
+    one and digitises the value list with these boundaries.
+
+    Parameters
+    ----------
+    values : list of floats
+        List of values to be binned.
+    max_bins : integer
+        Maximum number of bins. If there are less distinct value, every value
+        is in it's own bin.
+
+    Returns
+    -------
+    boundaries : array of floats
+        The value of the boundaries of the bins.
+    bins_digits : numpy array of integers
+        Array of the same length as the value list giving for each value the
+        bin number (between 1 and nb_of_bins) it belongs to.
+
+    """
+
+    if max_bins > len(values):
+        max_bins = len(values)
+
+    # The bin boundaries are the nb_of_bins + 1 quantiles.
+    quantiles = np.linspace(0, 1, max_bins + 1)
+    boundaries = stats.mstats.mquantiles(values, quantiles)
+
+    # Because of the way np.digitize works, we must have the last boundary
+    # higher than the value maximum to have this maximum belong to the last
+    # bin.
+    digitize_boundaries = np.copy(boundaries)
+    digitize_boundaries[-1] += 1
+    bin_digits = np.digitize(values, digitize_boundaries)
+
+    return boundaries, bin_digits
