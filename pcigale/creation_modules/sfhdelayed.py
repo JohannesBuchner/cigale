Correct galaxy_mass computation.
The "galaxy_mass" parameter must not be set to 1.0 in the stellar
emission modules because when using CIGALE to generate SED without
normalising the 1.0 is wrong and misleading.

Instead, we compute the real galaxy mass (the integration of the SFH) in
the star formation history modules.

pcigale/creation_modules/sfhdelayed.py
@@ -79,14 +79,18 @@ class SFHDelayed(CreationModule):
         # Main SFR
         sfr = sfr_A * time_grid / tau_main**2 * np.exp(-time_grid / tau_main)
 
-        # Normalise the SFH to 1 solar mass produced if asked to.
+        # Compute the galaxy mass and normalise the SFH to 1 solar mass
+        # produced if asked to.
+        galaxy_mass = np.trapz(sfr * 1e6, time_grid)
         if normalise:
-            sfr = sfr / np.trapz(sfr * 1e6, time_grid)
+            sfr = sfr / galaxy_mass
+            galaxy_mass = 1.
 
         sed.add_module(self.name, self.parameters)
 
         # Add the sfh and the output parameters to the SED.
         sed.sfh = (time_grid, sfr)
+        sed.add_info("galaxy_mass", galaxy_mass, True)
         sed.add_info("sfh.tau_main", tau_main)
 
 # CreationModule to be returned by get_module

pcigale/creation_modules/sfhfromfile.py
@@ -80,12 +80,16 @@ class SfhFromFile(CreationModule):
         sfr = sfr[time_grid <= age]
         time_grid = time_grid[time_grid <= age]
 
-        # Normalise the SFH to 1 solar mass produced if asked to.
+        # Compute the galaxy mass and normalise the SFH to 1 solar mass
+        # produced if asked to.
+        galaxy_mass = np.trapz(sfr * 1e6, time_grid)
         if normalise:
-            sfr = sfr / np.trapz(sfr * 1e6, time_grid)
+            sfr = sfr / galaxy_mass
+            galaxy_mass = 1.
 
         sed.add_module(self.name, self.parameters)
         sed.sfh = (time_grid, sfr)
+        sed.add_info("galaxy_mass", galaxy_mass, True)
         sed.add_info("sfh.id", sfr_column_number+1)
 
 # CreationModule to be returned by get_module