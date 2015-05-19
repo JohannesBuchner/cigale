21ab56ce3c32b5b7074dd3083e26a828f773c85f
Médéric Boquien <mboquien@ast.cam.ac.uk>
2015-05-19 13:53:14 +0200

    Correct two issues that caused plots to display an offset spectrum when redshifts were given with more than 2 decimals. First round the redshift to two decimals. Then, rather than recomputing the luminosity distance, let's just take the one from the best models file.

--- pcigale_plots/__init__.py
+++ pcigale_plots/__init__.py
@@ -122,8 +122,8 @@ def _sed_worker(obs, mod, filters, sed_type, nologo):
         obs_fluxes_err = np.array([obs[filt+'_err']
                                    for filt in filters.keys()])
         mod_fluxes = np.array([mod[filt] for filt in filters.keys()])
-        z = obs['redshift']
-        DL = max(10, cosmology.luminosity_distance(z).value * 1e6) * parsec
+        z = np.around(obs['redshift'], decimals=2)
+        DL = mod['universe.luminosity_distance']
 
         if sed_type == 'lum':
             xmin = PLOT_L_MIN
