20f67dd5ac27e7a9ce9521e76d83e41c07f17c1d
Denis Burgarella <denis.burgarella@lam.fr>
2015-07-31 14:30:14 +0200

    New SED creation modules
    - mbb: modified black body
    - param: parameterless module to compute some parameters and add them to
      the information dictionary
    - sfhcomb_constant and sfhcomb_delayed: two star formation histories

---
+++ pcigale/creation_modules/mbb.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Centre de données Astrophysiques de Marseille
+# Copyright (C) 2015 Institute of Astronomy, University of Cambridge
+# Licensed under the CeCILL-v2 licence - see Licence_CeCILL_V2-en.txt
+# Author: Denis Burgarella
+
+"""
+Modified Black Body module
+======================================
+
+This module implements a modified black body (MBB). This MMB can be 
+a stand-alone IR emission or it can come as an additional component. 
+The energy balance can be set to check the presence of a component 
+or not to account for the emission of a region completely embedded 
+in dust and not visible in the wavelength range.
+
+"""
+
+import numpy as np
+import scipy.constants as cst
+from collections import OrderedDict
+from . import CreationModule
+
+
+class MBB(CreationModule):
+    """One modified black body IR re-emission
+
+    Given an amount of attenuation (e.g. resulting from the action of a dust
+    attenuation module) this module normalises MBB plus any previous IR
+    contribution to this amount of energy. The final SED allows to keep the
+    energy balance or not..
+
+    """
+
+    parameter_list = OrderedDict([
+        ("epsilon_mbb", (
+            "float",
+            "Fraction [>= Ø] of L_dust(energy balance) in the MBB",
+            0.5
+        )),
+        ("t_mbb", (
+            "float",
+            "Temperature of black body in K.",
+            50.
+        )),
+        ("beta_mbb", (
+            "float",
+            "Emissivity index of modified black body.",
+            1.5
+        )),
+        ("energy_balance", (
+            "boolean",
+            "Energy balance checked?"
+            "If False, Lum[MBB] not taken into account in energy balance",
+            False
+        )),
+    ])
+
+    out_parameter_list = OrderedDict([
+        ("t_mbb", "Temperature of the modified black body in K."),
+        ("beta_mbb", "Emissivity index of the modified black body."),
+        ("epsilon_mbb", "Fraction of L_dust in the modified black body.")
+    ])
+
+    def _init_code(self):
+        """Build the model for a given set of parameters."""
+
+        epsilon = float(self.parameters["epsilon_mbb"])
+        T = float(self.parameters["t_mbb"])
+        beta = float(self.parameters["beta_mbb"])
+        
+        if epsilon < 0:
+           epsilon = 0.
+           print("epsilon_mbb must >= 0, we set epsilon_mbb = 0.0")
+        
+        # We define various constants necessary to compute the model. For
+        # consistency, we define the speed of light in nm s¯¹ rather than in
+        # m s¯¹.
+        c = cst.c * 1e9
+        lambda_0 = 200e3
+
+        self.wave = np.logspace(3., 6., 1000.)
+        conv = c / (self.wave * self.wave)
+
+        self.lumin_mbb = (conv * (1. - np.exp(-(lambda_0 / self.wave)
+                                ** beta)) * (c / self.wave) ** 3. / (np.exp(
+                                cst.h * c / (self.wave * cst.k * T)) - 1.))
+
+        # TODO, save the right normalisation factor to retrieve the dust mass
+        norm = np.trapz(self.lumin_mbb, x=self.wave)
+        self.lumin_mbb /= norm
+
+    def process(self, sed):
+        """Add the IR re-emission contributions.
+
+        Parameters
+        ----------
+        sed: pcigale.sed.SED object
+
+        """
+        if 'dust.luminosity' not in sed.info.keys():
+            sed.add_info('dust.luminosity', 1., True)
+        luminosity = sed.info['dust.luminosity']
+
+        sed.add_module(self.name, self.parameters)
+        sed.add_info("dust.t_mbb", self.parameters["t_mbb"])
+        sed.add_info("dust.beta_mbb", self.parameters["beta_mbb"])
+        sed.add_info("dust.epsilon_mbb", self.parameters["epsilon_mbb"])
+        epsilon = float(self.parameters["epsilon_mbb"])
+        energy_balance = (self.parameters["energy_balance"].lower() == "true")
+
+        if energy_balance:
+           # Since we can have another contribution to L_dust and the modified black body
+           # enters into the energy budget, energy balance, we have to save a new negative 
+           # component for each one, previously existing as:
+           # luminosity_other_balance = -luminosity_other * (1-epsilon); 
+           # epsilon being the contribution of the present MBB to L_dust.             
+           other_dust_contributions = [contrib for contrib in sed.contribution_names 
+                                       if "dust" in contrib]
+           for item in other_dust_contributions:
+              item_balance = item + '_balance'
+              lumin = sed.get_lumin_contribution(item)
+              wavelength = sed.wavelength_grid
+              sed.add_info(item_balance, 1., True)
+              sed.add_contribution(item_balance, wavelength, -lumin * epsilon)
+           # We read again the dust luminosities to check everything is OK.
+           #   lumin2 = sed.get_lumin_contribution(item_balance)
+           #   print("residual", lumin, lumin2)
+
+        # If the modified black body does not enter into the energy budget, 
+        # we do not change the luminosity of other dust contributions.
+        # The luminosity of the modified black body L_MBB = epsilon * L_dust. 
+        # The total dust luminosity will be : L_dust(inside energy budget) + L_MBB.
+         
+        # We add the contribution of the MBB to L_dust.
+        sed.add_contribution('dust.mbb', self.wave,
+                             luminosity * epsilon * self.lumin_mbb)
+#
+# CreationModule to be returned by get_module
+Module = MBB
---
+++ pcigale/creation_modules/sfhcomb_constant.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 Centre de données Astrophysiques de Marseille
+# Licensed under the CeCILL-v2 licence - see Licence_CeCILL_V2-en.txt
+# Author: Denis Burgarella
+
+"""
+SFH resembling a Dirac Comb formed by several regularly-spaced short constant SF events.
+========================================================================================
+
+This module implements a star formation history (SFH) formed by several 
+regularly-spaced short constant SF events.
+
+"""
+
+import numpy as np
+from collections import OrderedDict
+from . import CreationModule
+
+# Time lapse used in the age grid in Myr. If should be consistent with the
+# time lapse in the SSP modules.
+AGE_LAPSE = 1
+
+
+class SfhComb(CreationModule):
+    """Several regularly-spaced short constant SF events
+
+    This module sets the SED star formation history (SFH) as a combination of
+    several regularly-spaced short constant SF events.
+
+    """
+
+    parameter_list = OrderedDict([
+        ("N_events", (
+            "integer",
+            "Number of individual star formation events. ",
+            5
+        )),
+        ("t_duration", (
+            "integer",
+            "Length of each individual star formation event [Myr]. "
+            "Warning: if too long => continuous SFH",
+            20
+        )),
+        ("age", (
+            "integer",
+            "Age of the main stellar population in the galaxy in Myr."
+            "The precision is 1 Myr.",
+            1000
+        )),
+        ("age_last", (
+            "integer",
+            "Time since the end of the last SF event [Myr]. "
+            "Warning: Depending on the parameters, the last burst might not be finished. "
+            "The precision is 1 Myr.",
+            100
+        )),
+         ("sfr_A", (
+            "float",
+            "Strength of each SF event in M_sun/yr (useful if not normalised)",
+            1.
+        )),
+        ("normalise", (
+            "boolean",
+            "Normalise the SFH to produce one solar mass.",
+            "True"
+        )),
+    ])
+
+    out_parameter_list = OrderedDict([
+        ("N_events", "Number of individual star formation events"),
+        ("t_duration", "Length of each individual star formation event "
+                      "in Myr."),
+        ("age", "Age of the stellar population in the galaxy in Myr."),
+        ("age_last", "Time since the end of the last SF event in Myr."),
+        ("sfr_A", "Height of each SF event in M_sun/yr.")
+    ])
+
+    def process(self, sed):
+        """Add a star formation history formed by several regularly-spaced short SF events.
+
+        ** Parameters **
+        
+        sed: pcigale.sed.SED object
+
+        """
+        N_events = int(self.parameters["N_events"])
+        t_duration = int(self.parameters["t_duration"])
+        age = int(self.parameters["age"])
+        age_last = int(self.parameters["age_last"])
+        sfr_A = int(self.parameters["sfr_A"])
+        normalise = (self.parameters["normalise"].lower() == "true")
+
+        # Time grid and age. If needed, the age is rounded to the inferior Myr
+        time_grid = np.arange(AGE_LAPSE, age + AGE_LAPSE, AGE_LAPSE)
+        age = np.max(time_grid)
+        sfr = np.zeros(len(time_grid))
+
+        # Build the Dirac comb of events.
+        # Ages of the sequence of SF events in Myr, 
+        # N_events over "age" with the last one starting at "age_last"
+        time_event = np.linspace(0., age-age_last, num = N_events)
+        #print("time_event", time_event)
+
+        for i_time in range(N_events):
+           sfr[time_event[i_time]:time_event[i_time]+t_duration] = sfr_A
+        #print("sfr", sfr)
+
+        # Compute the galaxy mass and normalise the SFH to 1 solar mass
+        # produced if asked to.
+        galaxy_mass = np.trapz(sfr * 1e6, time_grid)
+        if normalise:
+            sfr = sfr / galaxy_mass
+            galaxy_mass = 1.
+
+        sed.add_module(self.name, self.parameters)
+
+        # Add the sfh and the output parameters to the SED.
+        sed.sfh = (time_grid, sfr)
+        sed.add_info("galaxy_mass", galaxy_mass, True)
+        sed.add_info("sfh.N_events", N_events)
+        sed.add_info("sfh.t_duration", t_duration)
+        sed.add_info("sfh.age_last", age_last)
+        #sed.add_info("age", age)
+
+# CreationModule to be returned by get_module
+Module = SfhComb
