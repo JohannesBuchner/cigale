2c699c7b2ed848a8ec9278271aedcb1a8ae17c64
Médéric Boquien <mederic.boquien@uantof.cl>
2015-11-04 11:34:03 -0300

    Ensure that the Lyman continuum emission is properly absorbed by the gas in the nebular module. This means that without that module, the Lyman continuum is left untouched. As this may be an issue for people used to running without the nebular module (assuming that the flux in individual bands in small), add an option to only absorb the Lyman continuum while discarding any nebular emission.

--- pcigale/creation_modules/dustatt_calzleit.py
+++ pcigale/creation_modules/dustatt_calzleit.py
@@ -259,7 +259,9 @@ class CalzLeit(CreationModule):
                                             powerlaw_slope)
 
         attenuation_total = 0.
-        for contrib in list(sed.contribution_names):
+        contribs = [contrib for contrib in sed.contribution_names if
+                    'absorption' not in contrib]
+        for contrib in contribs:
             age = contrib.split('.')[-1].split('_')[-1]
             luminosity = sed.get_lumin_contribution(contrib)
             attenuated_luminosity = (luminosity * 10 **
