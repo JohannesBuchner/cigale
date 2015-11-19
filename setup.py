deb7dc524e7c02d7359bb52b74d99fd6156170f6
Denis Burgarella <denis.burgarella@lam.fr>
2015-11-19 12:03:25 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ entry_points = {
 
 setup(
     name="pcigale",
-    version="0.6.0",
+    version="0.7.0",
     packages=find_packages(exclude=["database_builder"]),
 
     install_requires=['numpy', 'scipy', 'sqlalchemy', 'matplotlib',
